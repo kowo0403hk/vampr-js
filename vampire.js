27411155a1604f4c40a51a00249b2892f1155f3d
Alex Cheung <alexc.sfu@gmail.com>
2022-05-21 05:51:16 +0000

    added updated vampire.js

--- vampire.js
+++ vampire.js
@@ -10,22 +10,54 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let amount = 0;
+    let currentVamp = this;
+    while (currentVamp.creator) {
+      amount++;
+      currentVamp = currentVamp.creator;
+    }
+    return amount;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let currentVamp = this;
+    let currentAmount = currentVamp.numberOfVampiresFromOriginal;
+    let compareAmount = vampire.numberOfVampiresFromOriginal;
 
+    if (currentAmount < compareAmount) {
+      return true; 
+    } else {
+      return false;
+    }
+  }
+
+  /** Tree traversal methods **/
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    
   }
 
   /** Stretch **/
